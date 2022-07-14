3f251605fd66cb8fddc70d858a9ca86be913c2a5
RuiTakao <ruia1082halfnc@gmail.com>
2022-07-15 07:39:24 +0900

    投稿の編集ページ　レイアウト

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,17 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<section>
-    <div class="container">
-        <form action="{{ route('post.update', ['post' => $post->id]) }}" method="post">
+<section class="article_create_section">
+    <div class="container article_create_container">
+        <form class="article_create_form" action="{{ route('post.store') }}" method="post">
             @csrf
-            @method('PATCH')
-            <input type="text" name="title" value="{{ old('title', $post->title) }}">
-            <input type="text" name="sub_title" value="{{ old('sub_title', $post->sub_title) }}">
-            <textarea name="body" id="" cols="30" rows="10" value="{{ old('body', $post->body) }}"></textarea>
-            <input type="submit" value="編集する">
+            <div class="article_create_form_content">
+                <div class="idea-title_content">
+                    <label for="title">アイデアのタイトル</label>
+                    <input type="text" id="title" name="title" placeholder="アイデアのタイトルを入力してください" value="{{ old('title', $post->title) }}">
+                </div>
+                <div class="idea-sub-title_content">
+                    <label for="sub_title">アイデアの概要（一覧ページに表示されます）</label>
+                    <textarea name="sub_title" id="sub_title" cols="30" rows="10" placeholder="アイデア一覧ページに表示される内容です">{{ $post->sub_title }}</textarea>
+                </div>
+                <div class="idea-body_content">
+                    <label for="body">アイデアの詳細</label>
+                    <textarea name="body" id="body" cols="30" rows="10" placeholder="アイデアの詳細を入力してください">{{ $post->body }}</textarea>
+                </div>
+            </div>
+            <div class="article_create_btn">
+                <input type="submit" value="投稿する">
+            </div>
         </form>
-        <a href="{{ route('posts.index') }}">戻る</a>
     </div>
 </section>
 @endsection
